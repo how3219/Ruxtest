0251fd9570c23eb5efa8f9035d628870c6ee01e0
kimin <kic3210@gmail.com>
2021-05-20 18:02:28 +0900

    배너 api 적용

--- src/components/main_slide.js
+++ src/components/main_slide.js
@@ -1,64 +1,82 @@
-import React, {Component,useState} from 'react';
-import {View, Image, TouchableOpacity, Dimensions} from 'react-native';
+import React, {Component, useState} from 'react';
+import {View, Image, TouchableOpacity, Dimensions, Alert} from 'react-native';
 
 import Carousel from 'react-native-snap-carousel';
 import styles from '../style/style';
-
+import API_CALL from '../ApiCall';
 export const Width = Dimensions.get('window').width - 65;
 
 export default class MainSlide extends React.Component {
-
-    constructor(props){
-                    super(props);
-                    this.state = {
-                      activeIndex:0,
-                      MainItems: [
-                {
-                    id: 1,
-                    image: require('../images/main_slide01.png'),
-                },
-                {
-                    id:  2,
-                    image: require('../images/main_slide02.png'),
-                },
-            ]
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeIndex: 0,
+      MainItems: [],
     };
-
-    _renderItem = ({item, index}) => {
-        return(
-            <View style={{marginBottom:15,height:254,}}>
-                  <TouchableOpacity 
-                  style={{
-                    flex:1,
-                    justifyContent: 'flex-start',
-                    alignItems: 'flex-start',
-                  }}>
-                        <Image
-                        key={index}
-                        source={item.image}
-                        style={{resizeMode:'cover',width:'100%',height:'100%'}}
-                        />
-                  </TouchableOpacity>
-            </View>
-        );
-    };
-
-    render () {
-        return(
-        <View style={{ flex: 1, flexDirection:'row', justifyContent: 'center', }}>
-            <Carousel
-                loop={true}
-                autoplay={false}
-                layout={"default"}
-                ref={ref => this.carousel = ref}
-                data={this.state.MainItems}
-                renderItem={this._renderItem}
-                sliderWidth={Width}
-                itemWidth={Width}
+  }
+  componentDidMount = async () => {
+    try {
+      const form = new FormData();
+      form.append('method', 'proc_main_banner');
+      const url = 'http://dmonster1566.cafe24.com';
+      const path = '/json/proc_json.php';
+      const api = await API_CALL(url + path, form, false);
+      const {
+        data: {result, item},
+      } = api;
+      if (result === '0') return Alert.alert('no result Slide');
+      if (result === '1') {
+        this.setState({MainItems: item});
+      }
+    } catch (e) {
+      Alert.alert('slide Catch');
+    }
+  };
+  _renderItem = ({item, index}) => {
+    let bn_begin = new Date(item.bn_begin);
+    let bn_end = new Date(item.bn_end);
+    let current = new Date();
+    console.log(1111, bn_begin);
+    console.log(2222, bn_end);
+    console.log(3333, current);
+    // console.log(current);
+    // let currentdata = this.date.
+    console.log(bn_begin < current);
+    console.log(595959, current < bn_end);
+    return (
+      bn_begin < current &&
+      current < bn_end && (
+        <View style={{marginBottom: 15, height: 254}}>
+          <TouchableOpacity
+            style={{
+              flex: 1,
+              justifyContent: 'flex-start',
+              alignItems: 'flex-start',
+            }}>
+            <Image
+              key={index}
+              source={{uri: item.mt_img}}
+              style={{resizeMode: 'cover', width: '100%', height: '100%'}}
             />
+          </TouchableOpacity>
         </View>
-        );
-    };
+      )
+    );
+  };
 
-};
+  render() {
+    return (
+      <View style={{flex: 1, flexDirection: 'row', justifyContent: 'center'}}>
+        <Carousel
+          autoplay={false}
+          layout={'default'}
+          ref={ref => (this.carousel = ref)}
+          data={this.state.MainItems}
+          renderItem={this._renderItem}
+          sliderWidth={Width}
+          itemWidth={Width}
+        />
+      </View>
+    );
+  }
+}
