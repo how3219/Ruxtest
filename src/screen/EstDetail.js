7ee84a6beb2a2adb4844b057a06ad0f313624c73
kimin <how3219@gmail.com>
2021-05-25 00:41:53 +0900

    견적 상세 정보 view 완료

--- src/screen/EstDetail.js
+++ src/screen/EstDetail.js
@@ -1,14 +1,72 @@
-import React, {useState} from 'react';
-import {SafeAreaView, View, ScrollView, TouchableOpacity, Text, StyleSheet,Modal} from 'react-native';
-
+import React, {useState,useEffect} from 'react';
+import {SafeAreaView, View, ScrollView, TouchableOpacity, Text, StyleSheet,Modal, Alert, TextInput} from 'react-native';
 import Header, {DetailHead} from '../components/header';
 import Footer from '../components/footer';
 import Product from '../components/product';
-
+import API_CALL from '../ApiCall';
 import Icon from 'react-native-vector-icons/Ionicons';
-
-const EstDetail = ({navigation}) => {
+const url = 'http://dmonster1566.cafe24.com'
+const path = '/json/proc_json.php'
+const EstDetail = (props) => {
   const [isvisible, setIsvisible] = useState(false);
+  const [detailItem,setDetailItem] = useState([])
+  const [price,setPrice] = useState('')
+  const {route:{params},navigation} = props;
+  useEffect(() => {
+    getEstimateDetail()
+  }, [])
+  const getEstimateDetail = async() => {
+    try{
+      const form = new FormData;
+      form.append('method','proc_my_estimate_detail')
+      form.append('idx',params.idx)
+      
+      const api = await API_CALL(url+path, form, true)
+      const {data:{result,item}} = api;
+
+      if(result === "0") return Alert.alert("title","failed")
+      if(result === "1"){
+        
+        props.route.params = {...props.route.params,...item[0]}
+        setPrice(item[0].td_price)
+        setDetailItem(item[0])
+      }
+    }catch(e){
+      Alert.alert('제목','EstimateDetail')
+    }
+  }
+  const cancleEstimate = async() => {
+    try{
+        const form = new FormData;
+        form.append('method','proc_my_estimate_cancle')
+        form.append('idx',detailItem.idx)
+        const api = await API_CALL(url+path, form, true)
+        const {data:{result,message}} = api;
+        if(result==='0'){ Alert.alert('title',message)}
+        else if(result==='1'){
+          navigation.goBack();
+        }
+    }catch(e){
+
+    }
+  }
+  const estmateModify = async() => {
+    try{
+      const form = new FormData;
+      form.append('method','proc_my_estimate_modify')
+      form.append('idx',detailItem.idx)
+      form.append('td_price',price)
+      form.append('pt_deal_type',detailItem.pt_deal_type)
+      const api = await API_CALL(url+path, form, true)
+      const {data:{result,message}} = api;
+      if(result==='0'){ Alert.alert('title',message)}
+      else if(result==='1'){
+        
+      }
+  }catch(e){
+
+  }
+  }
   return(
     <SafeAreaView style={{flex:1,backgroundColor: '#fff'}}>
       <DetailHead title="견적 상세 정보"/>
@@ -16,7 +74,7 @@ const EstDetail = ({navigation}) => {
       {/* 내 견적*/}
         <View style={{padding:20,}}>
           <View style={styles.estcont}>
-            <Product/>
+            <Product {...props}/>
           </View>
           <View style={styles.estbox}>
             <Text style={{borderBottomWidth:1,borderBottomColor:'#eee',padding:12,fontSize:16,fontFamily:'NotoSansKR-Bold',lineHeight:20, }}>
@@ -25,19 +83,25 @@ const EstDetail = ({navigation}) => {
             <View style={{padding:12,}}>
               <View style={styles.textbox}>
                 <Text style={styles.texta}>입찰금액</Text>
-                <Text style={styles.texta}>420,000</Text>
+                <Text style={styles.textb}>{price}</Text>
               </View>
               <View style={styles.textbox}>
                 <Text style={styles.texta}>거래유형</Text>
-                <Text style={styles.textb}>직거래</Text>
+                <Text style={styles.textb}>{detailItem.pt_deal_type}</Text>
               </View>
               <View style={{flex:1,flexDirection:'row',justifyContent:'space-between',alignItems: 'center'}}>
                 <Text style={styles.texta}>입찰입시</Text>
-                <Text style={styles.textb}>2021.01.16 11:23</Text>
+                <Text style={styles.textb}>{detailItem?detailItem.td_wdate:0}</Text>
               </View>
             </View>
             <TouchableOpacity
-            onPress={()=> navigation.navigate('EstCheck')}
+            onPress={()=> navigation.navigate('EstCheck',{
+              idx:detailItem.idx,
+              pt_title: detailItem.pt_title,
+              pt_selling_edate: detailItem.pt_selling_edate,
+              dday: detailItem.dday,
+              pt_image1: detailItem.pt_image1,
+            })}
             style={{backgroundColor:'#EBEBEB',height:44,alignItems: 'center',justifyContent:'center',}}>
               <Text style={{fontSize:16,fontFamily:'NotoSansKR-Medium'}}>견적서 보기</Text>
             </TouchableOpacity>
@@ -47,7 +111,7 @@ const EstDetail = ({navigation}) => {
                 onPress={() => setIsvisible(true)}>
                     <Text style={styles.btncont}>견적 수정</Text>
                 </TouchableOpacity>
-                <TouchableOpacity style={styles.btnbox}>
+                <TouchableOpacity style={styles.btnbox} onPress={()=>cancleEstimate()}>
                     <Text style={styles.btncont}>견적 취소</Text>
                 </TouchableOpacity>
                 <TouchableOpacity style={{flex:1,backgroundColor:'#477DD1',height:57,justifyContent: 'center',alignItems: 'center'}}>
@@ -123,7 +187,8 @@ const EstDetail = ({navigation}) => {
                 justifyContent:'center',
                 alignItems:'center',
                 margin:5,
-              }}>
+              }}
+              onPress={()=>estmateModify()}>
                 <Text style={{
                   fontSize:13,
                   fontFamily:'NotoSansKR-Regular',
