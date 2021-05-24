a255a1065b466f8e14b5e1f6593016cda8a5a442
kimin <how3219@gmail.com>
2021-05-25 00:46:54 +0900

    구매내역과 정산내역이 달라서 구분(확인예정)

--- src/components/transitem.js
+++ src/components/transitem.js
@@ -1,95 +1,24 @@
 import React from 'react';
 import {FlatList, TouchableOpacity, View, Image, Text,} from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons'
-
-const TransList = () => {
-    const transItem = [
-        {
-            id:1,
-            image: require('../images/profile01.png'),
-            title: 'MAISON MARGIELA',
-            price: '200,000원',
-            user: 'nappeni',
-            location: '샤넬 종로점',
-            date: '2021.03.08',
-        },
-        {
-            id:2,
-            image: require('../images/profile02.png'),
-            title: 'MAISON MARGIELA',
-            price: '300,000원',
-            user: 'nappeni',
-            location: '샤넬 종로점',
-            date: '2021.03.08',
-        },
-        {
-            id:3,
-            image: require('../images/profile03.png'),
-            title: 'MAISON MARGIELA',
-            price: '300,000원',
-            user: 'nappeni',
-            location: '샤넬 종로점',
-            date: '2021.03.08',
-        },
-        {
-            id:4,
-            image: require('../images/profile_no_img.png'),
-            title: 'MAISON MARGIELA',
-            price: '300,000원',
-            user: 'nappeni',
-            location: '샤넬 종로점',
-            date: '2021.03.08',
-        },
-        {
-            id:5,
-            image: require('../images/profile_no_img.png'),
-            title: 'MAISON MARGIELA',
-            price: '300,000원',
-            user: 'nappeni',
-            location: '샤넬 종로점',
-            date: '2021.03.08',
-        },
-        {
-            id:6,
-            image: require('../images/profile_no_img.png'),
-            title: 'MAISON MARGIELA',
-            price: '300,000원',
-            user: 'nappeni',
-            location: '샤넬 종로점',
-            date: '2021.03.08',
-        },
-        {
-            id:7,
-            image: require('../images/profile_no_img.png'),
-            title: 'MAISON MARGIELA',
-            price: '300,000원',
-            user: 'nappeni',
-            location: '샤넬 종로점',
-            date: '2021.03.08',
-        },
-        {
-            id:8,
-            image: require('../images/profile_no_img.png'),
-            title: 'MAISON MARGIELA',
-            price: '300,000원',
-            user: 'nappeni',
-            location: '샤넬 종로점',
-            date: '2021.03.08',
-        },
-    ]
+import { useNavigation } from '@react-navigation/native';
+const TransList = (props) => {
+    const {item:itemlist} = props;    
     return(
         <FlatList
             style={{flex:1}}
-            data={transItem}
+            data={itemlist}
             renderItem={({item, index} ) => <TransItem item={item}/>}
-            keyExtractor={(item) => `${item.id}`}
+            keyExtractor={(item) => `${item.idx}`}
             showsVerticalScrollIndicator={false}
         />
     );
 };
 
 function TransItem({item : transItem}){
+    const navigation = useNavigation();
     return(
+        transItem?
         <TouchableOpacity 
             style={{
                 flexDirection:'row',
@@ -99,6 +28,10 @@ function TransItem({item : transItem}){
                 borderBottomColor:'#eee',
                 paddingVertical:14,
                 }}
+                onPress={()=>transItem&&transItem.list&&navigation.navigate('OrderInformation',{
+                    idx:transItem&&!transItem.list?transItem.idx:transItem&&transItem.list&&transItem.list.idx,
+                    pt_deal_type:transItem&&!transItem.list?transItem.pt_deal_type:transItem&&transItem.list&&transItem.list.pt_deal_type,
+                })}
         >
             <TouchableOpacity
                 style={{width:20,height:20,position:'absolute',top:4,right:0,}}
@@ -108,16 +41,17 @@ function TransItem({item : transItem}){
             <View style={{width:62,height:62,borderRadius:31,overflow:'hidden',marginRight:10,}}>
                 <Image 
                     style={{resizeMode:'cover',width:'100%',height:'100%',}}
-                    source={transItem.image}
+                    source={{uri:transItem&&!transItem.list?transItem.pt_image1:transItem.list.pt_image1}}
                 />
             </View>
             <View style={{flexGrow:1}}>
-                <Text style={{fontSize:16,fontFamily:'NotoSansKR-Bold',lineHeight:20,paddingBottom:4,}}>{transItem.title}</Text>
-                <Text style={{fontFamily:'NotoSansKR-Medium',lineHeight:16,fontSize:13,}}>구매가 <Text style={{fontFamily:'NotoSansKR-Regular',color:'#555'}}>{transItem.price}</Text></Text>
+                <Text style={{fontSize:16,fontFamily:'NotoSansKR-Bold',lineHeight:20,paddingBottom:4,width:150}}
+                numberOfLines={1}>{transItem&&!transItem.list?transItem.pt_title:transItem.list.pt_title}</Text>
+                <Text style={{fontFamily:'NotoSansKR-Medium',lineHeight:16,fontSize:13,}}>구매가 <Text style={{fontFamily:'NotoSansKR-Regular',color:'#555'}}>{transItem&&!transItem.list?transItem.td_price:transItem.list.td_price}</Text></Text>
                 <View style={{flexDirection:'row',alignItems:'center'}}>
-                    <Text style={{fontSize:13,fontFamily:'NotoSansKR-Medium',lineHeight:20,color:'#5E5E5E',paddingRight:6,}}>{transItem.user}</Text>
-                    <Text style={{fontSize:12,fontFamily:'NotoSansKR-Regular',lineHeight:20,color:'#B9B9B9',paddingRight:6,}}>{transItem.location}</Text>
-                    <Text style={{fontSize:12,fontFamily:'NotoSansKR-Regular',lineHeight:20,color:'#B9B9B9'}}>{transItem.date}</Text>
+                    <Text style={{fontSize:13,fontFamily:'NotoSansKR-Medium',lineHeight:20,color:'#5E5E5E',paddingRight:6,}}>{transItem&&!transItem.list?transItem.mt_nickname:transItem.list.mt_nickname}</Text>
+                    <Text style={{fontSize:12,fontFamily:'NotoSansKR-Regular',lineHeight:20,color:'#B9B9B9',paddingRight:6,}}>{transItem&&!transItem.list?transItem.mt_company_name:transItem.list.mt_company_name}</Text>
+                    <Text style={{fontSize:12,fontFamily:'NotoSansKR-Regular',lineHeight:20,color:'#B9B9B9'}}>{transItem&&!transItem.list?transItem.td_date:transItem.list.td_date}</Text>
                 </View>
             </View>
             <TouchableOpacity 
@@ -129,9 +63,9 @@ function TransItem({item : transItem}){
                     justifyContent:'center',
                     alignItems:'center',
                 }}>
-                <Text style={{fontFamily:'NotoSansKR-Medium',color:'#fff',fontSize:13,lineHeight:20,}}>카드결제</Text>
+                <Text style={{fontFamily:'NotoSansKR-Medium',color:'#fff',fontSize:13,lineHeight:20,}}>{transItem&&!transItem.list?transItem.payment_type:transItem.list.payment_type}</Text>
             </TouchableOpacity>
-        </TouchableOpacity>
+        </TouchableOpacity>:null
     );
 };
 
